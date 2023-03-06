8f83ed8b845cf36fd222bf9352f227bc52d2f107
azim fakhruddin <mohammad.azim@checkpointspot.asia>
2023-03-06 12:30:39 +0800

    done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Patch, Req} from '@nestjs/common';
+import { Request } from 'express';
 
-@Controller()
+@Controller({})
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Patch('/:userId')
+  update(@Req() req: Request) {
+    return req.body;
   }
 }
