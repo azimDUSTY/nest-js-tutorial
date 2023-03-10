c84be93d2cd1d72013fa06834164ec3c1788030e
azim fakhruddin <mohammad.azim@checkpointspot.asia>
2023-03-10 11:51:01 +0800

    add crud user

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,60 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private userService: UserService) {}
 
+  /**
+   * Get all user
+   * @returns array of user objects
+   */
   @Get()
   async users() {
     return await this.userService.findAll();
+  }
+
+  /**
+   * Create user
+   * @param body
+   * @returns return object of a user
+   */
+  @Post()
+  async createUser(@Body() body) {
+    return await this.userService.createData(body);
+  }
+
+  /**
+   * Update user
+   * @param id
+   * @param body
+   * @returns return object of a user
+   */
+  @UsePipes(ValidationPipe)
+  @Patch('/:id')
+  async updateUser(@Param('id', ParseIntPipe) id, @Body() body) {
+    return await this.userService.updateData(id, body);
+  }
+
+  /**
+   * Delete user
+   * @param id
+   * @returns return object of a user
+   */
+  @UsePipes(ValidationPipe)
+  @Delete('/:id')
+  async deleteUser(@Param('id', ParseIntPipe) id) {
+    return await this.userService.deleteUser(id);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -8,4 +8,25 @@ export class UserService {
   async findAll() {
     return await this.dbService.user.findMany();
   }
+
+  async createData(data: any) {
+    return await this.dbService.user.create({ data });
+  }
+
+  async updateData(id: number, data: any) {
+    return await this.dbService.user.update({
+      data,
+      where: {
+        id,
+      },
+    });
+  }
+
+  async deleteUser(id: number) {
+    return await this.dbService.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
